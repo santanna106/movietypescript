ed14ea58cb5a8ee8d54849c9ad1d0918794e898e
Gabriel Andrade <gabriel.santana@gmail.com>
2023-07-11 03:37:57 -0300

    configuration of the morgan to manger loggin of the http reques of the application

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,14 @@ import db from '@config/db';
 
 import Logger from '@config/logger';
 
+import morganMiddleware from '@middleware/morganMiddleware';
+
 const app = express();
 
+app.use(morganMiddleware);
 app.use(express.json());
 app.use("/api/",router);
+
 
 const port = config.get<number>("port");
 
